1082e595b9e76bd7b00a6fcbe2e14b55352ae29c
jig10 <jigodoyrubin@gmail.com>
2020-02-11 07:11:17 +0000

    added images and copy in landing

--- src/js/component/MainSelectionJumbotron.js
+++ src/js/component/MainSelectionJumbotron.js
@@ -1,10 +1,11 @@
 import React from "react";
 //import { Link } from "react-router-dom";
+//import Save from "../../img/Jumboimg.jpg";
 
 export const MainSelectionJumbotron = () => {
 	return (
 		<div className="jumbotron">
-			<h3 className="display-3">Ultra Fast Road Assistance</h3>
+			<h3 className="display-3">Stranded and Feeling Helpless?</h3>
 			{/*<p className="lead">Please choose the service you need</p>
 			<hr className="my-2" />*/}
 		</div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,12 +2,12 @@ import React from "react";
 import { MainSelectionJumbotron } from "../component/MainSelectionJumbotron";
 import { ServiceCards } from "../component/ServiceCards";
 
-import "../../styles/home.scss";
+import "../../styles/home.css";
 import Navbar from "../component/navbar";
 
 export const Home = () => (
 	<div>
-		<div className="text-center mt-5">
+		<div className="text-center">
 			<MainSelectionJumbotron />
 			<ServiceCards />
 		</div>
