8927766789b2b4f75e19f4760a3157313e25e049
jig10 <jigodoyrubin@gmail.com>
2020-02-13 03:46:13 +0000

    added doggie img

--- src/js/component/RequestPage.js
+++ src/js/component/RequestPage.js
@@ -3,50 +3,56 @@ import { Context } from "../store/appContext";
 import React, { useContext, useState } from "react";
 import { CheckoutCard } from "./CheckoutCard";
 import "../../styles/RequestCheckoutPage.css";
+import image from "../../img/doggie.png";
 
 export const Request = () => {
 	const { store, actions } = useContext(Context);
 	const [state, setState] = useState(false);
 	return (
-		<div className="text-center">
-			<span className="buttonspace">
-				<button
-					type="button"
-					className="btn btn-primary btn-lg"
-					onClick={() => {
-						{
-							actions.updateServiceType("gas");
+		<div>
+			<div className="text-center">
+				<span className="buttonspace">
+					<button
+						type="button"
+						className="btn btn-primary btn-lg"
+						onClick={() => {
+							{
+								actions.updateServiceType("gas");
+								setState(true);
+							}
+						}}>
+						Gas
+					</button>
+				</span>
+				<span className="buttonspace">
+					<button
+						type="button"
+						className="btn btn-primary btn-lg"
+						onClick={() => {
+							{
+								actions.updateServiceType("flat_tire");
+								setState(true);
+							}
+						}}>
+						Flat Tire
+					</button>
+				</span>
+				<span className="buttonspace">
+					<button
+						type="button"
+						className="btn btn-primary btn-lg"
+						onClick={() => {
+							actions.updateServiceType("battery");
 							setState(true);
-						}
-					}}>
-					Gas
-				</button>
-			</span>
-			<span className="buttonspace">
-				<button
-					type="button"
-					className="btn btn-primary btn-lg"
-					onClick={() => {
-						{
-							actions.updateServiceType("flat_tire");
-							setState(true);
-						}
-					}}>
-					Flat Tire
-				</button>
-			</span>
-			<span className="buttonspace">
-				<button
-					type="button"
-					className="btn btn-primary btn-lg"
-					onClick={() => {
-						actions.updateServiceType("battery");
-						setState(true);
-					}}>
-					Battery
-				</button>
-			</span>
-			<div className="checkoutspace">{state && <CheckoutCard />}</div>
+						}}>
+						Battery
+					</button>
+				</span>
+				<div className="checkoutspace">{state && <CheckoutCard />}</div>
+			</div>
+			<div>
+				<img src={image} style={{ height: "204px", width: "auto", left: "42%", position: "absolute" }} />
+			</div>
 		</div>
 	);
 };
