3b82295edc2fc5e7385476a5bb4d52e0140a56ec
bharathichinnusamy <lcsbharathi@gmail.com>
2020-02-13 05:45:07 +0000

    final change

--- src/js/component/RequestPage.js
+++ src/js/component/RequestPage.js
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext";
 import React, { useContext, useState } from "react";
 import { CheckoutCard } from "./CheckoutCard";
 import "../../styles/RequestCheckoutPage.css";
+import image from "../../img/doggie.png";
 
 export const Request = () => {
 	const { store, actions } = useContext(Context);
@@ -47,6 +48,9 @@ export const Request = () => {
 				</button>
 			</span>
 			<div className="checkoutspace">{state && <CheckoutCard />}</div>
+            <div>
+				<img src={image} style={{ height: "204px", width: "auto", left: "42%", position: "absolute" }} />
+			</div>
 		</div>
 	);
 };
